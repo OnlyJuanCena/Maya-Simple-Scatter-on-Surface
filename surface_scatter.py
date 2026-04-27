e4b7441fd0c9e1271401d6cd77cf97b9f4fee585
JuanMAC <jjgarchomp@gmail.com>
2026-04-27 04:16:33 -0500

    Added initial psuedocode

---
+++ surface_scatter.py
@@ -0,0 +1,6 @@
+# create mesh plane?
+# divide mesh into points
+# get location of points and put into a list
+# create group of duplicate meshes and place on points
+	# use dictionaries to associate mesh with point
+# hide duplicate meshes at random based on density slider
