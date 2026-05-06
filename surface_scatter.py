bf6a5033b9f5a896d3c560fe6b9a667d5da4ea86
JuanMAC <jjgarchomp@gmail.com>
2026-05-06 18:09:10 -0500

    Debugged get_objects()

--- surface_scatter.py
+++ surface_scatter.py
@@ -3,6 +3,7 @@ import maya.OpenMayaUI as omui
 from PySide6 import QtWidgets, QtCore
 from shiboken6 import wrapInstance
 
+
 def get_maya_main_win():
     """Return the Maya main window"""
     main_win_addr = omui.MQtUtil.mainWindow()
@@ -11,32 +12,34 @@ def get_maya_main_win():
 # idea: create mesh plane?
 # divide mesh into points
 
+
 # get objects in scene that can be scattered on
 def get_objects():
     objects = cmds.ls(geometry=True)
-    return objects[1]
+    print(objects)
+    return objects[0]
+
 
 # get location of points and put into a dictionary
-def get_points():
+# def get_points():
+#     object = get_objects()
+#     print(object)
+#     cmds.select(object.vtx[5:10])
 
-    selected_verts = cmds.ls(selection=True, flatten=True)
+#     selected_verts = cmds.polyListComponentConversion(object, toVertex=True)
 
-    if len(cmds.ls(selection=True)) == 0:
-        print("You need to select an object or group first.")
+#     vert_positions = {}
+#     for vert in selected_verts:
+#         pos = cmds.xform(vert, query=True, worldSpace=True, translation=True)
+#         vert_positions[vert] = pos
 
-    vert_positions = {}
-    for vert in selected_verts:
-        pos = cmds.xform(vert, query=True, worldSpace=True, translation=True)
-        vert_positions[vert] = pos
-
-    print(vert_positions)
-    
-	
+#     print(vert_positions)
 
 # create group of duplicate meshes and place on points
-	# use dictionaries to associate mesh with point
+# use dictionaries to associate mesh with point
 # hide duplicate meshes at random based on density slider
 
+
 if __name__ == "__main__":
-    # get_objects()
-    get_points()
+    get_objects()
+    # get_points()
