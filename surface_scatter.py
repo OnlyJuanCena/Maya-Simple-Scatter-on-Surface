ce5437da1a28504be91d05f01008affa149fae97
JuanMAC <jjgarchomp@gmail.com>
2026-05-06 21:39:21 -0500

    Refactored scatter functions into Class

--- surface_scatter.py
+++ surface_scatter.py
@@ -9,35 +9,37 @@ def get_maya_main_win():
     main_win_addr = omui.MQtUtil.mainWindow()
     wrapInstance(int(main_win_addr), QtWidgets.QWidget)
 
-# idea: create mesh plane?
 
+class SimpleScatter():
 
-def get_objects():
-    objects = cmds.ls(geometry=True)
-    return objects[0]
+    # idea: create mesh plane?
 
+    def get_objects(self):
+        objects = cmds.ls(geometry=True)
+        selected_object = objects[0]
+        return selected_object
 
-def get_points():
-    object = get_objects()
-    object_verts = f"{object}.vtx[*]"
+    def get_points(self):
+        obj = self.get_objects()
+        object_verts = f"{obj}.vtx[*]"
 
-    selected_verts = cmds.ls(object_verts, flatten=True)
+        selected_verts = cmds.ls(object_verts, flatten=True)
 
-    vert_positions = {}
-    for vert in selected_verts:
-        pos = tuple(cmds.xform(vert,
-                               query=True,
-                               worldSpace=True,
-                               translation=True))
-        vert_positions[vert] = pos
+        vert_positions = {}
+        for vert in selected_verts:
+            pos = tuple(cmds.xform(vert,
+                                   query=True,
+                                   worldSpace=True,
+                                   translation=True))
+            vert_positions[vert] = pos
 
-    return vert_positions
+        return vert_positions
 
-# create group of duplicate meshes and place on points
-# use dictionaries to associate mesh with point
-# hide duplicate meshes at random based on density slider
+    # create group of duplicate meshes and place on points
+    # use dictionaries to associate mesh with point
+    # hide duplicate meshes at random based on density slider
 
 
 if __name__ == "__main__":
-    # get_objects()
-    get_points()
+    scatter = SimpleScatter()
+    print(scatter.get_points())
